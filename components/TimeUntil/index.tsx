81af2651be80d4db321a0d8d6304c38f6eb7601b
jimmynames <jim.me.ryan@gmail.com>
2020-08-19 09:54:44 +0100

    jump to use state

--- components/TimeUntil/index.tsx
+++ components/TimeUntil/index.tsx
@@ -6,14 +6,14 @@ interface Props {
   destination: string;
 }
 
-const workOutTime = (data) => {
+const workOutTime = (data, i) => {
   console.log('items wasnt undefined', data)
   // console.log('items.all', data.all)
   let itemsAll = data
   console.log('itemsAll', itemsAll)
   const result = data.filter(item => item.destination_name = "London Euston");
   console.log('result:', result);
-  let first = result[0]
+  let first = result[i]
   console.log('first', first)
 
   // Arrival Time
@@ -44,31 +44,39 @@ const workOutTime = (data) => {
     console.log('totalTimeUntilTrain', totalTimeUntilTrain)
     timeUntilTrain = totalTimeUntilTrain
     console.log('timeUntilTrain in func:', timeUntilTrain)
+    // setTimeUntilArrival(timeUntilTrain)
+    return (
+      <div><h1>{`${timeUntilTrain} minutes until the next train to London Euston`}</h1></div>
+    );
     // return timeUntilTrain
   } else if (arrivalH == h && arrivalM == m){
     console.log('arrived!')
-    // setHasArrived(true)
-    setTimeout(() => workOutTime(data), 1100)
+    // this.state.setHasArrived(true)
+    setTimeout(() => workOutTime(data, 1), 3000)
     return (
       <div><h1>{`The train is at the platform`}</h1></div>
     );
   } else if (arrivalH == h && m > arrivalM) {
     console.log('train has gone!')
-    setTimeout(() => workOutTime(data), 1100)
+    setTimeout(() => workOutTime(data, 1), 3000)
     return (
-      <div><h1>{`You just missed it, either refresh or wait a minute to see when the next one is`}</h1></div>
+      <div><h1>{`You just missed it, refresh to see when the next one is`}</h1></div>
+    );
+  } else if (arrivalH == h && (arrivalM - m == 1)) {
+    return (
+      <div><h1>{`1 minute until the next train arrives`}</h1></div>
     );
   } else {
     console.log('same hour')
     const minutesLeft = arrivalM - m
     console.log('minutes Left:', minutesLeft)
     timeUntilTrain = minutesLeft
+    return (
+      <div><h1>{`${timeUntilTrain} minutes until the next train`}</h1></div>
+    );
   }
   console.log('timeUntilTrain:', timeUntilTrain)
   // return timeUntilTrain
-  return (
-    <div><h1>{`${timeUntilTrain} minutes until the next train to London Euston`}</h1></div>
-  );
 }
 
 
@@ -78,6 +86,7 @@ function TimeUntil(): JSX.Element {
   const [hasWorked, setHasWorked] = useState(false);
   const [hasError, setHasError] = useState(false)
   const [hasArrived, setHasArrived] = useState(false)
+  const [timeUntilArrival, setTimeUntilArrival] = useState(0)
   const [items, setItems] = useState([]);
 
 
@@ -107,7 +116,7 @@ function TimeUntil(): JSX.Element {
     return <div>Loading...</div>;
   } else if (isLoaded && hasWorked) {
       let time
-      return workOutTime(items.all)
+      return workOutTime(items.all, 0)
     } else {
       return (
         <h1>no data loaded</h1>
