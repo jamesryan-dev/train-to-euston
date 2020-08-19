dd374640bb122622851eda9f4d5f6623d852ccd4
jimmynames <jim.me.ryan@gmail.com>
2020-08-19 19:12:23 +0100

    deploy001

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import TimeUntil from '../components/TimeUntil'
 import RenderTrains from '../components/RenderTrains'
-import Clock from '../components/Clock'
+
 function Home(): JSX.Element {
   return (
     <div className="container">
