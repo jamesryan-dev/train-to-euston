cfefd8a1ac163f50f10c72e3d130b18bdf634ae7
jimmynames <jim.me.ryan@gmail.com>
2020-08-21 17:40:31 +0100

    bottom slider

--- components/BottomSticker/index.js
+++ components/BottomSticker/index.js
@@ -1,10 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import {BottomStickerComp, StickerComp} from './styled'
 
 function BottomSticker () {
+  const [showBottomSticker, setShowBottomSticker] = useState(false);
+
+  const handleShowBottomSticker = () => {
+    if (showBottomSticker) {
+      setShowBottomSticker(false)
+    } else {
+      setShowBottomSticker(true)
+    }
+  }
   return (
     <BottomStickerComp>
-      <StickerComp>
-        <h3>Made by <span>namesjame.es</span></h3>
+      <StickerComp onClick={handleShowBottomSticker} show={showBottomSticker}>
+        <div className='bottom-header'>
+          <h3>Made by <span>namesjame.es</span></h3>
+          <div>chevron</div>
+          <h3>Add to homepage</h3>
+        </div>
       </StickerComp>
     </BottomStickerComp>
   )
--- components/BottomSticker/styled.ts
+++ components/BottomSticker/styled.ts
@@ -6,7 +6,7 @@ export const BottomStickerComp = styled.div`
   width: 100%;
   position: fixed;
   top: 0;
-  z-index: 0;
+  z-index: ${(props) => props.show ? '0' : '2'};
 `
 
 export const StickerComp = styled.div`
@@ -14,12 +14,26 @@ export const StickerComp = styled.div`
   bottom: 0;
   left: 50%;
   height: 30vh;
-  transform: translateY(80%) translateX(-50%);
+  height: ${(props) => props.show ? '90vh' : '30vh'};
+  box-shadow: ${(props) => props.show ? '0 7px 13px rgba(11,31,29, 0.19), 0 4px 5px rgba(11,31,29, 0.23)' : null};
+  transform: ${(props) => props.show ? 'translateY(0%) translateX(-50%)' : 'translateY(80%) translateX(-50%)'};
   width: 90%;
   background: #004c45;
+  background: #0d4a48;
+  background: ${(props) => props.show ? 'blue' : 'red'};
   padding: 0.4rem 0.6rem;
   padding-top: 1rem;
   border-radius: 10px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+  transition: all 1s;
+
+  z-index: 5;
+  .bottom-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
   h3 {
     span {
       text-decoration: underline;
